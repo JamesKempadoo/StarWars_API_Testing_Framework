21e516327097c22682665f308473c73735d2e03f
JamesKempadoo <44989874+JamesKempadoo@users.noreply.github.com>
2022-11-17 14:40:27 +0000

    link checker

--- src/test/java/org/mtfbwy/spartanapi/framework/services/Util.java
+++ src/test/java/org/mtfbwy/spartanapi/framework/services/Util.java
@@ -1,4 +1,15 @@
 package org.mtfbwy.spartanapi.framework.services;
 
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Util  {
+    public static boolean linkChecker(List<String> list, String request) {
+        for (String item : list) {
+            if (!Pattern.matches("https://swapi.dev/api/"+request+"/+\\d+/", item)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
