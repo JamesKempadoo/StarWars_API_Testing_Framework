ac1074487ce624576464889ac74f19a18b6dfaec
JamesKempadoo <44989874+JamesKempadoo@users.noreply.github.com>
2022-11-17 15:14:38 +0000

    Update Util.java

--- src/test/java/org/mtfbwy/spartanapi/framework/services/Util.java
+++ src/test/java/org/mtfbwy/spartanapi/framework/services/Util.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class Util  {
-    public static boolean linkChecker(List<String> list, String request) {
+    public static boolean linkChecker(List<String> list, Endpoint endpoint) {
         for (String item : list) {
-            if (!Pattern.matches("https://swapi.dev/api/"+request+"/+\\d+/", item)) {
+            if (!Pattern.matches("https://swapi.dev/api/"+endpoint.label+"/+\\d+/", item)) {
                 return true;
             }
         }
