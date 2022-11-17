3d0f7b507de76facf7e8a7c93cd029a970ed3557
FranciscoCavaco <fg_cavaco@hotmail.com>
2022-11-17 12:40:45 +0000

    added connection managerÂ

--- src/test/java/org/mtfbwy/spartanapi/framework/connection/ConnectionManager.java
+++ src/test/java/org/mtfbwy/spartanapi/framework/connection/ConnectionManager.java
@@ -1,4 +1,58 @@
 package org.mtfbwy.spartanapi.framework.connection;
 
+import org.mtfbwy.spartanapi.framework.services.Endpoints;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConnectionManager  {
+    private static final String BASEURL = "https://swapi.dev/api/";
+
+    public static String getConnection(){
+        return BASEURL;
+    }
+
+//    public static String getConnection(enum endpoint){
+//        String result = (BASEURL + endpoint.value());
+//        return result;
+//    }
+
+    public static String getConnection(String endpoint, String key, String value) {
+        return BASEURL + endpoint + "" + "?" + key + "=" + value;
+    }
+
+    public static String getConnection(String endpoint, String key, int value) {
+        return BASEURL + endpoint + "" + "?" + key + "=" + value;
+    }
+
+    private static HttpResponse<String> getResponse(){
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest
+                .newBuilder()
+                .uri(URI.create(BASEURL))
+                .build();
+        HttpResponse<String> response = null;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    public static int getStatusCode(){
+        return getResponse().statusCode();
+    }
+
+    public static String getHeader(String key){
+
+        return getResponse()
+                .headers()
+                .firstValue(key)
+                .orElse("Key Not Found");
+    }
 }
--- src/test/java/org/mtfbwy/spartanapi/framework/services/Endpoints.java
+++ src/test/java/org/mtfbwy/spartanapi/framework/services/Endpoints.java
@@ -1,2 +1,5 @@
-package org.mtfbwy.spartanapi.framework.services;public enum Endpoints {
+package org.mtfbwy.spartanapi.framework.services;
+
+public enum Endpoints {
+    PLANETS, PEOPLE, FILMS, SPECIES, VEHICLES, STARSHIPS
 }
