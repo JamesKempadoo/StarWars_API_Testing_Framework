10b31ec8e5ad671e7e195ee9cf211682073277c2
JamesKempadoo <44989874+JamesKempadoo@users.noreply.github.com>
2022-11-18 14:48:30 +0000

    films dto tests

--- src/test/java/org/mtfbwy/spartanapi/framework/FilmsAPITest.java
+++ src/test/java/org/mtfbwy/spartanapi/framework/FilmsAPITest.java
@@ -1,7 +1,9 @@
 package org.mtfbwy.spartanapi.framework;
 
+import io.cucumber.java.bs.A;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mtfbwy.spartanapi.framework.connection.ConnectionManager;
 import org.mtfbwy.spartanapi.framework.dto.FilmDTO;
@@ -14,7 +16,7 @@ public class FilmsAPITest {
     static FilmDTO dto;
     static FilmsRepository dtoRepo;
     static int status;
-    static String urlEndPointDTO = ConnectionManager.getConnection(Endpoint.FILMS, 6);
+    static String urlEndPointDTO = ConnectionManager.getConnection(Endpoint.FILMS, 1);
     static String urlEndPointRepo = ConnectionManager.getConnection(Endpoint.FILMS);
 
     @BeforeAll
@@ -27,16 +29,29 @@ public class FilmsAPITest {
     }
 
     @Test
-    void testRun() {
-        //System.out.println(dto.toString());
-
-        System.out.println(dtoRepo);
-
-        // Checking that actor exists in PEOPLE endpoint
-        Assertions.assertTrue(dto.checkIfIdExistsInList(Endpoint.PEOPLE, 1));
-
-
+    @DisplayName("Check that has value 1")
+    void checkThatHasValue1(){
+        Assertions.assertTrue(dto.checkIfIdExistsInList(Endpoint.PEOPLE,1));
     }
+
+    @Test
+    @DisplayName("Check that starships has value of 2")
+    void checkThatStarshipsHasValueOf2(){
+        Assertions.assertTrue(dto.checkIfIdExistsInList(Endpoint.STARSHIPS,2));
+    }
+
+    @Test
+    @DisplayName("Check that planets has value of 1")
+    void checkThatPlanetsHasValueOf1(){
+        Assertions.assertTrue(dto.checkIfIdExistsInList(Endpoint.PLANETS,1));
+    }
+
+    @Test
+    @DisplayName("Check that vehicles has value of 4")
+    void checkThatVehiclesHasValueOf4(){
+        Assertions.assertTrue(dto.checkIfIdExistsInList(Endpoint.VEHICLES,4));
+    }
+
 
 
 }
